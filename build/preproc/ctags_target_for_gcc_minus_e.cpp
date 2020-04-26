f3f77643e431ec44de5f1d032190bcddc14b35a1
Jens Gassmann <jg@gassmann.it>
2020-04-26 14:04:01 +0200

    Custom font for digits

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -70,6 +70,41 @@ int lastButtonState = 1; // previous state of the button
 # 69 "/home/jens/Dropbox/ESP8266/followercounter/followercounter/followercounter.ino" 2
 
 
+
+const uint8_t DotMatrixCondensed[990] __attribute__((section(".text." "DotMatrixCondensed"))) =
+  "\340\0\3\2\3\3\1\3\5\4\10\0\0\10\0\0\0\0\303\1C\3\301 \4@f!\4@f\42"
+  "\4@f#\4@f$\4@f%\4@f&\4@f'\4@f(\4@f)\4@f*"
+  "\4@f+\4@f,\4@f-\4@f.\4@f/\4@f\60\13\374\326(\221eH\244"
+  "D\1\61\10\373\322$R\227\1\62\12\374\326(Q\26\265\15\1\63\13\374\326(Q\226\210R\242\0\64"
+  "\12\374\326EJ\42\15Z\1\65\12\374V\14\332\230I\211\2\66\12\374V\265-\221\224(\0\67\11\374"
+  "V\14Y\251V\2\70\14\374\326(\221\224(\221\224(\0\71\12\374\326(\221\224lQ\11:\6\331K"
+  "$\1;\4@f<\4@f=\4@f>\4@f\77\4@f@\4@fA\4@fB\4"
+  "@fC\4@fD\4@fE\4@fF\4@fG\4@fH\4@fI\4@fJ\4"
+  "@fK\4@fL\4@fM\4@fN\4@fO\4@fP\4@fQ\4@fR\4"
+  "@fS\4@fT\4@fU\4@fV\4@fW\4@fX\4@fY\4@fZ\4"
+  "@f[\4@f\134\4@f]\4@f^\4@f_\4@f`\4@fa\4@fb\4"
+  "@fc\4@fd\4@fe\4@ff\4@fg\4@fh\4@fi\4@fj\4"
+  "@fk\4@fl\4@fm\4@fn\4@fo\4@fp\4@fq\4@fr\4"
+  "@fs\4@ft\4@fu\4@fv\4@fw\4@fx\4@fy\4@fz\4"
+  "@f{\4@f|\4@f}\4@f~\4@f\177\4@f\200\4@f\201\4@f\202\4"
+  "@f\203\4@f\204\4@f\205\4@f\206\4@f\207\4@f\210\4@f\211\4@f\212\4"
+  "@f\213\4@f\214\4@f\215\4@f\216\4@f\217\4@f\220\4@f\221\4@f\222\4"
+  "@f\223\4@f\224\4@f\225\4@f\226\4@f\227\4@f\230\4@f\231\4@f\232\4"
+  "@f\233\4@f\234\4@f\235\4@f\236\4@f\237\4@f\240\4@f\241\4@f\242\4"
+  "@f\243\4@f\244\4@f\245\4@f\246\4@f\247\4@f\250\4@f\251\4@f\252\4"
+  "@f\253\4@f\254\4@f\255\4@f\256\4@f\257\4@f\260\4@f\261\4@f\262\4"
+  "@f\263\4@f\264\4@f\265\4@f\266\4@f\267\4@f\270\4@f\271\4@f\272\4"
+  "@f\273\4@f\274\4@f\275\4@f\276\4@f\277\4@f\300\4@f\301\4@f\302\4"
+  "@f\303\4@f\304\4@f\305\4@f\306\4@f\307\4@f\310\4@f\311\4@f\312\4"
+  "@f\313\4@f\314\4@f\315\4@f\316\4@f\317\4@f\320\4@f\321\4@f\322\4"
+  "@f\323\4@f\324\4@f\325\4@f\326\4@f\327\4@f\330\4@f\331\4@f\332\4"
+  "@f\333\4@f\334\4@f\335\4@f\336\4@f\337\4@f\340\4@f\341\4@f\342\4"
+  "@f\343\4@f\344\4@f\345\4@f\346\4@f\347\4@f\350\4@f\351\4@f\352\4"
+  "@f\353\4@f\354\4@f\355\4@f\356\4@f\357\4@f\360\4@f\361\4@f\362\4"
+  "@f\363\4@f\364\4@f\365\4@f\366\4@f\367\4@f\370\4@f\371\4@f\372\4"
+  "@f\373\4@f\374\4@f\375\4@f\376\4@f\377\4@f\0\0\0\4\377\377\0";
+
+
 //define your default values here, if there are different values in config.json, they are overwritten.
 char instagramName[40];
 char matrixIntensity[5];
@@ -313,7 +348,7 @@ void infoIP() {
 void infoVersion() {
 
   char versionString[8];
-  sprintf(versionString,"Ver. %s", "1.9rc1");
+  sprintf(versionString,"Ver. %s", "1.9rc2");
 
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_finderskeepers_tf);
@@ -422,7 +457,7 @@ void updateFirmware() {
     ESPhttpUpdate.onProgress(update_progress);
     ESPhttpUpdate.onError(update_error);
 
-    t_httpUpdate_return ret = ESPhttpUpdate.update(client, "https://counter.buuild.it/static/themes/counter/followercounter.ino.d1.bin");
+    t_httpUpdate_return ret = ESPhttpUpdate.update(client, "https://counter.buuild.it/static/themes/counter/followercounter.ino.bin");
 
 
     switch (ret) {
@@ -615,8 +650,8 @@ void printTime() {
 
 
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_finderskeepers_tf);
-  u8g2.drawStr(4,7,time_value);
+  u8g2.setFont(DotMatrixCondensed);
+  u8g2.drawStr(4,9,time_value);
   u8g2.sendBuffer();
 
 }
@@ -630,39 +665,39 @@ void printCurrentFollower() {
 
     if (follower > 0 ) {
 
-    if ( follower > 9999 ) {
+      if ( follower > 9999 ) {
 
-      u8g2.clearBuffer();
-      u8g2.setFont(u8g2_font_finderskeepers_tf);
-      u8g2.drawStr(1,7,copy);
-      u8g2.sendBuffer();
+        u8g2.clearBuffer();
+        u8g2.setFont(DotMatrixCondensed);
+        u8g2.drawStr(1,9,copy);
+        u8g2.sendBuffer();
 
-    } else {
+      } else {
 
-      u8g2.clearBuffer();
+        u8g2.clearBuffer();
 
 
-      // Äußerer Rahmen Insta Logo
-      u8g2.drawHLine(1,0,6);
-      u8g2.drawHLine(1,7,6);
+        // Äußerer Rahmen Insta Logo
+        u8g2.drawHLine(1,0,6);
+        u8g2.drawHLine(1,7,6);
 
-      u8g2.drawLine(0,1,0,6);
-      u8g2.drawLine(7,1,7,6);
+        u8g2.drawLine(0,1,0,6);
+        u8g2.drawLine(7,1,7,6);
 
-      // Innererer Rahmen Insta Logo
-      u8g2.drawHLine(3,2,2);
-      u8g2.drawHLine(3,5,2);
+        // Innererer Rahmen Insta Logo
+        u8g2.drawHLine(3,2,2);
+        u8g2.drawHLine(3,5,2);
 
-      u8g2.drawLine(2,3,2,4);
-      u8g2.drawLine(5,3,5,4);
+        u8g2.drawLine(2,3,2,4);
+        u8g2.drawLine(5,3,5,4);
 
-      u8g2.drawPixel(6,1);
+        u8g2.drawPixel(6,1);
 
-      // Anzahl Follower 
-      u8g2.setFont(u8g2_font_finderskeepers_tf);
-      u8g2.drawStr(10,7,copy);
-      u8g2.sendBuffer();
+        // Anzahl Follower 
+        u8g2.setFont(DotMatrixCondensed);
+        u8g2.drawStr(10,9,copy);
+        u8g2.sendBuffer();
 
-    }
+      }
     }
 }
